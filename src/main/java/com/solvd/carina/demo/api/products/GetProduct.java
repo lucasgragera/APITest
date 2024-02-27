2043cebb5af2d881f69581d25d95deab952f1758
Lucas Gragera <lucasgragera1@gmail.com>
2024-02-27 19:45:48 -0300

    APITest

---
+++ src/main/java/com/solvd/carina/demo/api/products/GetProduct.java
@@ -0,0 +1,24 @@
+package com.solvd.carina.demo.api.products;
+
+import com.zebrunner.carina.api.AbstractApiMethodV2;
+import com.zebrunner.carina.api.annotation.Endpoint;
+import com.zebrunner.carina.api.annotation.RequestTemplatePath;
+import com.zebrunner.carina.api.annotation.ResponseTemplatePath;
+import com.zebrunner.carina.api.annotation.SuccessfulHttpStatus;
+import com.zebrunner.carina.api.http.HttpMethodType;
+import com.zebrunner.carina.api.http.HttpResponseStatusType;
+import com.zebrunner.carina.utils.config.Configuration;
+
+@Endpoint(url = "https://dummyjson.com/products/${index}", methodType = HttpMethodType.GET)
+
+
+@SuccessfulHttpStatus(status = HttpResponseStatusType.OK_200)
+public class GetProduct extends AbstractApiMethodV2 {
+    public GetProduct() {
+        replaceUrlPlaceholder("base_url", Configuration.getRequired("api_url"));
+    }
+    public void setIndex(){
+        int index = 2;
+        replaceUrlPlaceholder("index", String.valueOf(index));
+    }
+}
---
+++ src/test/java/com/solvd/carina/demo/APIHWTest.java
@@ -0,0 +1,39 @@
+package com.solvd.carina.demo;
+
+import com.fasterxml.jackson.core.JsonToken;
+import com.jayway.jsonpath.JsonPath;
+import com.solvd.carina.demo.api.products.GetProduct;
+import com.solvd.carina.demo.api.products.PostProduct;
+import com.solvd.carina.demo.api.products.PutProduct;
+import com.zebrunner.carina.core.IAbstractTest;
+import com.zebrunner.carina.core.registrar.ownership.MethodOwner;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+public class APIHWTest implements IAbstractTest {
+    @Test
+    @MethodOwner(owner = "Lucas")
+    public void getProducts(){
+        GetProduct getProduct = new GetProduct();
+        getProduct.setIndex();
+        getProduct.callAPIExpectSuccess();
+        getProduct.validateResponseAgainstSchema("api/products/_get/rs.schema");
+    }
+    @Test
+    @MethodOwner(owner = "Lucas")
+    public void postProducts(){
+        PostProduct postProduct = new PostProduct();
+        postProduct.setProperties("api/products/product.properties");
+        postProduct.callAPIExpectSuccess();
+        postProduct.validateResponse();
+    }
+    @Test
+    @MethodOwner(owner = "Lucas")
+    public void putProducts(){
+        PutProduct putProduct = new PutProduct();
+        putProduct.setIndex();
+        putProduct.setProperties("api/products/product.properties");
+        putProduct.callAPIExpectSuccess();
+        putProduct.validateResponse();
+    }
+}
